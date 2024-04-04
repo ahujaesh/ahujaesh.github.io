d62ff629480a476c5e54561108c72e3b2fdcbfc0
Eshaan Ahuja <86844958+ahujaesh@users.noreply.github.com>
2024-04-03 21:00:56 -0500

    This should work better (#8)

--- scripts.php
+++ scripts.php
@@ -10,12 +10,21 @@ function unlock() {
         <br>
         <p><a href="https://docs.google.com/document/d/16nSAIckEvsOb4Y2V06vTWUbwHBNEwPJhKD9KQ-prIrk/edit" target="_blank">This</a> is our current script as of <strong>12/12/2023</strong>. We keep changing documents, so this might not be up to date.<p>
         <br>
+        <p><a href="https://docs.google.com/document/d/16nSAIckEvsOb4Y2V06vTWUbwHBNEwPJhKD9KQ-prIrk/edit" target="_blank">This</a> is our current script as of <strong>12/12/2023</strong>. We keep changing documents, so this might not be up to date. Note: editing has been disabled for this one.<p>
+        <br>
         <p><a href="https://docs.google.com/document/d/1iP4MYPHe0nbUpYcOKLLLvW--nIOs1OjZ35Q4yDPD7cQ/edit" target="_blank">This</a> is our current script as of <strong>11/18/2023</strong>. We keep changing documents, so this might not be up to date. Inspiration: <a href="https://www.archpaper.com/2022/09/leah-wulfman-explorations-buildings-digital-garbage/" target="_blank" title="inspiration" alt="inspiration"> here</a>.</p>
         <br>
         <br>
         <p><a href="https://docs.google.com/document/d/1q3oYlRN_FkQnQmVnYWNGKpR9AXxPmie-Mr2tx5EeoTA/edit" target="_blank">This</a> is our current script as of <strong>11/4/2023</strong>. We keep changing documents, so this might not be up to date.<p>
         <br>
-        <p>If you want to see our scripts from previous seasons, you can go to <a href="https://docs.google.com/document/d/1QVIf4FFdRv8RRuElcoPNe1rXGXcz1pc6Iboib0kiJZE/edit">here</a> to see our script from the 2021 - 2022 season and <a href="https://docs.google.com/document/d/1h9WDSNLww4X41hdqXvIKaBW-eDTWYUVZZBk1Zhxe2Dg/edit">here</a> to see our script for the 2022 - 2023 season.</p>
+        <p>If you want to see our scripts from previous seasons, you can go to <a href="https://docs.google.com/document/d/1QVIf4FFdRv8RRuElcoPNe1rXGXcz1pc6Iboib0kiJZE/edit">here</a> to see our script from the 2021 - 2022 season and <a href="https://docs.google.com/document/d/1h9WDSNLww4X41hdqXvIKaBW-eDTWYUVZZBk1Zhxe2Dg/edit">here</a> to see our script for the 2022 - 2023 season.
     </div>';
 }
 ?>
+
+<script>
+    // Attaching the unlock function to the window object
+    window.unlock = function() {
+        <?php unlock(); ?>
+    };
+</script>
