8dda9ecd4dd8ad7977395b65d3065a78d60ea9d7
Eshaan Ahuja <86844958+ahujaesh@users.noreply.github.com>
2024-04-04 12:10:23 -0500

    Begin #9

---
+++ sets.php
@@ -0,0 +1,5 @@
+<?php
+function unlock() {
+    echo '<div id="classified"><p><a href="https://docs.google.com/document/d/1iP4MYPHe0nbUpYcOKLLLvW--nIOs1OjZ35Q4yDPD7cQ/edit?disco=AAAA_s2zRJc" target="_blank">This</a> is our current document as of 11/20/2023. We keep changing documents, so this might not be up to date.<p> <p>Building and designing is ongoing, but for security, you need to go the above <a href="https://docs.google.com/document/d/1iP4MYPHe0nbUpYcOKLLLvW--nIOs1OjZ35Q4yDPD7cQ/edit?disco=AAAA_s2zRJc" target="_blank">link</a> to view the plans and names.</p></div>';
+}
+?>
