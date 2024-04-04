825b7cc794245d691d4a9ecab7b3d296e69c1398
Eshaan Ahuja <86844958+ahujaesh@users.noreply.github.com>
2024-04-03 20:53:55 -0500

    Testing #8

---
+++ scripts.php
@@ -0,0 +1,21 @@
+<?php
+function unlock() {
+    echo '<div id="classified">
+        <p>Script writing progress is going well, there are many current documents, but the final one will be here once finished.</p>
+        <p><a href="https://docs.google.com/document/d/1xqHA-H1fZqalz8aySLGv5I6OZ1IOh2PHt-CH-RqJlwY/edit" target="_blank">This</a> is our current script as of <strong>3/31/2024</strong>. We keep changing documents, so this might not be up to date. (This is the current Pre-states one)<p>
+        <br>
+        <p><a href="https://docs.google.com/document/d/171QXSXwrPgYnH1qeM2JlP3HFqsiCaHxaelqEYhbW2KQ/edit#heading=h.tutk6vhew98r" target="_blank">This</a> is our current script as of <strong>3/14/2024</strong>. We keep changing documents, so this might not be up to date. Note: <a href="https://docs.google.com/document/d/16nSAIckEvsOb4Y2V06vTWUbwHBNEwPJhKD9KQ-prIrk/edit" target="_blank">this</a> is now archived and a backup for reference.<p>
+        <br>
+        <p>Update: <a href="https://docs.google.com/document/d/16nSAIckEvsOb4Y2V06vTWUbwHBNEwPJhKD9KQ-prIrk/edit" target="_blank">this</a> is still the current script as of <strong>3/8/2024</strong>.</p>
+        <br>
+        <p><a href="https://docs.google.com/document/d/16nSAIckEvsOb4Y2V06vTWUbwHBNEwPJhKD9KQ-prIrk/edit" target="_blank">This</a> is our current script as of <strong>12/12/2023</strong>. We keep changing documents, so this might not be up to date.<p>
+        <br>
+        <p><a href="https://docs.google.com/document/d/1iP4MYPHe0nbUpYcOKLLLvW--nIOs1OjZ35Q4yDPD7cQ/edit" target="_blank">This</a> is our current script as of <strong>11/18/2023</strong>. We keep changing documents, so this might not be up to date. Inspiration: <a href="https://www.archpaper.com/2022/09/leah-wulfman-explorations-buildings-digital-garbage/" target="_blank" title="inspiration" alt="inspiration"> here</a>.</p>
+        <br>
+        <br>
+        <p><a href="https://docs.google.com/document/d/1q3oYlRN_FkQnQmVnYWNGKpR9AXxPmie-Mr2tx5EeoTA/edit" target="_blank">This</a> is our current script as of <strong>11/4/2023</strong>. We keep changing documents, so this might not be up to date.<p>
+        <br>
+        <p>If you want to see our scripts from previous seasons, you can go to <a href="https://docs.google.com/document/d/1QVIf4FFdRv8RRuElcoPNe1rXGXcz1pc6Iboib0kiJZE/edit">here</a> to see our script from the 2021 - 2022 season and <a href="https://docs.google.com/document/d/1h9WDSNLww4X41hdqXvIKaBW-eDTWYUVZZBk1Zhxe2Dg/edit">here</a> to see our script for the 2022 - 2023 season.</p>
+    </div>';
+}
+?>
